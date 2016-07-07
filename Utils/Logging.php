fd63bde84f42e3ea6e1fd8bbe8ede9c89a77c7b0
Kacper Donat <kadet1090@gmail.com>
2016-07-08 00:32:55 +0200

    add logging utility

---
+++ Utils/Logging.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * XMPP Library
+ *
+ * Copyright (C) 2016, Some right reserved.
+ *
+ * @author Kacper "Kadet" Donat <kacper@kadet.net>
+ *
+ * Contact with author:
+ * Xmpp: me@kadet.net
+ * E-mail: contact@kadet.net
+ *
+ * From Kadet with love.
+ */
+
+namespace Kadet\Xmpp\Utils;
+
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+trait Logging
+{
+    private static $global = null;
+
+    /** @var LoggerInterface */
+    private $logger = null;
+
+    /**
+     * @return LoggerInterface
+     */
+    public function getLogger()
+    {
+        if($this->logger === null) {
+            $this->logger = self::get();
+        }
+
+        return $this->logger;
+    }
+
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public static function set(LoggerInterface $logger)
+    {
+        self::$global = $logger;
+    }
+
+    /**
+     * @return LoggerInterface
+     */
+    public static function get()
+    {
+        if(self::$global === null) {
+            self::$global = new NullLogger();
+        }
+
+        return self::$global;
+    }
+}
